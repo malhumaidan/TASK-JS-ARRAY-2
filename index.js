5c33de9ee794a9c461ff06bb349b5a361fcc07a8
Engxli <baaa555@outlook.com>
2023-03-07 17:05:14 +0300

    TASK-JS-ARRAY-2

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1. Declare an empty array called `myArray`.
+
+// 2. Create an array called 'fruits' that contains the following elements: 'apple', 'banana', 'cherry'.
+
+// 3. Access the second element of the fruits array and assign it to a variable called 'secondFruit'.
+
+// 4. Add the element 'orange' to the end of the 'fruits' array.
+
+// 5. Remove the first element from the 'fruits' array.
+
+// 6. Find the length of the 'fruits' array and assign it to a variable called 'numFruits'.
+
+// 7. Check if the element 'banana' exists in the 'fruits' array and assign the result to a variable called 'hasBanana'.
+
+// 8. Loop through the 'fruits' array and console.log each element.
+
+// 9. Create a new array called 'numbers' that contains the numbers 1, 2, 3, 4.
+
+// 10 Use the 'map' method to create a new array called 'squares' that contains the squares of each number in the 'numbers' array.
